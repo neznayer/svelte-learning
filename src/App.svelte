6ce6d75df0761372407ce11e6fbd39ec144887fe
Anton Zhuravlev <nokker@gmail.com>
2022-10-19 21:33:46 +0900

    grouped binding to variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,57 @@
 <script>
-	let yes = false;
+	let scoops = 1;
+	let flavours = ['Mint choc chip'];
+
+	function join(flavours) {
+		if (flavours.length === 1) return flavours[0];
+		return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
+	}
+
+	$: console.log({scoops})
+	$: console.log({flavours})
 </script>
 
+<h2>Size</h2>
+
 <label>
-	<input type="checkbox" bind:checked={yes} />
-	Yes! Send me regular email spam
+	<input type=radio bind:group={scoops} name="scoops" value={1}>
+	One scoop
 </label>
 
-{#if yes}
-	<p>
-		Thank you. We will bombard your inbox and sell
-		your personal details.
-	</p>
+<label>
+	<input type=radio bind:group={scoops} name="scoops" value={2}>
+	Two scoops
+</label>
+
+<label>
+	<input type=radio bind:group={scoops} name="scoops" value={3}>
+	Three scoops
+</label>
+
+<h2>Flavours</h2>
+
+<label>
+	<input type=checkbox bind:group={flavours} name="flavours" value="Cookies and cream">
+	Cookies and cream
+</label>
+
+<label>
+	<input type=checkbox bind:group={flavours} name="flavours" value="Mint choc chip22">
+	Mint choc chip
+</label>
+
+<label>
+	<input type=checkbox bind:group={flavours} name="flavours" value="Raspberry ripple">
+	Raspberry ripple
+</label>
+
+{#if flavours.length === 0}
+	<p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+	<p>Can't order more flavours than scoops!</p>
 {:else}
 	<p>
-		You must opt in to continue. If you're not
-		paying, you're the product.
+		You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
+		of {join(flavours)}
 	</p>
 {/if}
-
-<button disabled={!yes}>Subscribe</button>
