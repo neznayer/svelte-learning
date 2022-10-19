91638b3be6bde796b6ac23fc44e5559d334d66ba
Anton Zhuravlev <nokker@gmail.com>
2022-10-20 00:38:40 +0900

    onDestroy

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script>
-	function handleClick() {
-		alert('no more alerts')
-	}
+	import Timer from './Timer.svelte';
+	import Timer2 from './Timer2.svelte';
+
+	let open = false;
+	let seconds = 0;
+
+	const toggle = () => (open = !open);
+	const handleTick = () => (seconds += 1);
 </script>
 
-<button on:click|once={handleClick}>
-	Click me
-</button>
+<div>
+	<button on:click={toggle}
+		>{open ? 'Close' : 'Open'} Timer</button
+	>
+	<p>
+		The Timer component has been open for
+		{seconds}
+		{seconds === 1 ? 'second' : 'seconds'}
+	</p>
+	{#if open}
+		<Timer callback={handleTick} />
+		<Timer2 callback={handleTick} />
+	{/if}
+</div>
